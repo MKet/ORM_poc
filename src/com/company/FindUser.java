7eaf01d6eccb1914022b978d4e0d9eed5963c6e0
Marco Ketelaars <marco-ketelaars94@hotmail.com>
2018-05-09 12:33:23 +0200

    Find user with query parameters

---
+++ src/com/company/FindUser.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import com.company.Entities.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.List;
+import java.util.Scanner;
+
+public class FindUser {
+    public static void main(String[] args) {
+        User user = new User();
+
+        System.out.println("Vul je naam in");
+
+        Scanner scanner = new Scanner(System.in);
+        user.setName(scanner.nextLine());
+
+        System.out.println("Vul je wachtwoord in");
+
+        user.setPassword(scanner.nextLine());
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("manager1");
+
+        EntityManager entityManager = factory.createEntityManager();
+
+        User u = (User) entityManager.createQuery("from User u where u.name = :usename and u.password = :password")
+                .setParameter("username", user.getName())
+                .setParameter("password", user.getPassword())
+                .getSingleResult();
+
+        if (u != null) {
+            System.out.print("User found!");
+        }
+    }
+}
